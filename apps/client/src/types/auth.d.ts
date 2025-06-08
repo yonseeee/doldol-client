50dfb4c8b316e34461eaa94e7330f3bd3d6d2fd5
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-08 16:15:40 +0900

    feat(types): edit auth.d.ts

--- apps/client/src/types/auth.d.ts
+++ apps/client/src/types/auth.d.ts
@@ -1,38 +1,73 @@
-
-export interface RegisterRequest{
-  id:string;
-  password:string;
-  name:string;
-  phone:string;
-  email:string;
+// 자체 서비스 회원가입 POST /auth/register
+export interface RegisterRequest {
+  id: string;
+  password: string;
+  name: string;
+  phone: string;
+  email: string;
 }
 
-export interface AuthRegisterRequest{
-  name:string;
-  phone:string;
-  email:string;
-  socialId:string;
-  socialType:string;
+// 소셜 회원가입 POST /auth/oauth/register
+export interface OAuthRegisterRequest {
+  name: string;
+  phone: string;
+  email: string;
+  socialId: string;
+  socialType: string;
 }
 
-export interface EmailCodeVerifyRequest{
-  email:string;
-  code:string;
+// 이메일 인증 코드 검증 POST /auth/email/verify-code
+export interface EmailCodeVerifyRequest {
+  email: string;
+  code: string;
 }
 
-
-export interface EmailCodeSendRequest{
-  email:string;
+// 이메일 인증 코드 전송 POST /auth/email/send-code
+export interface EmailCodeSendRequest {
+  email: string;
 }
 
-export interface PhoneCheckRequest{
-  phone:string;
+// 휴대폰 번호 중복 검증 요청 POST /auth/check-phone
+export interface PhoneCheckRequest {
+  phone: string;
 }
 
-export interface IdCheckRequest{
-  id:string;
+// 아이디 중복 확인 POST /auth/check-id
+export interface IdCheckRequest {
+  id: string;
 }
 
-export interface EmailCheckRequest{
-  email:string;
+// 이메일 중복 확인 POST /auth/check-email
+export interface EmailCheckRequest {
+  email: string;
+}
+
+// 로그인
+export interface LoginRequest {
+  id: string;
+  password: string;
+}
+
+export interface LoginSuccessData {
+  userId: number;
+  role: string;
+}
+
+export interface ApiResponseLoginSuccess {
+  data: LoginSuccessData;
+  status: number;
+  message: string;
+}
+
+export interface LoginFailureResponse {
+  data: null;
+  code: string;
+  message: string;
+}
+
+// 로그아웃
+export interface LogoutResponse {
+  data: null;
+  status: number;
+  message: string;
 }
