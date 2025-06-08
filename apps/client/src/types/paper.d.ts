c294358cda183717c831cc257c50af49fcadecd2
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-08 16:16:10 +0900

    feat(types): edit paper.d.ts

--- apps/client/src/types/paper.d.ts
+++ apps/client/src/types/paper.d.ts
@@ -1,6 +1,6 @@
-import { MessageResponse } from './message';
+// import { MessageResponse } from './message';
 
-export interface RollingPaper {
+export interface Paper {
   paperId: number;
   name: string;
   description: string;
@@ -9,32 +9,62 @@ export interface RollingPaper {
   openDate: string;
 }
 
-export interface RollingPaperPagination {
-  data: RollingPaper[];
+// 롤링페이퍼 리스트 GET /papers
+export interface PaperPagination {
+  data: Paper[];
   size: number;
   nextCursor: number;
   hasNext: boolean;
   empty: boolean;
 }
 
-export interface PaperListResponse {
+export interface PaperListData {
   paperCount: number;
-  rollingPaper: RollingPaperPagination;
+  rollingPaper: PaperPagination;
 }
 
+export interface ApiResponsePaperListResponse {
+  data: PaperListData;
+  status: number;
+  message: string;
+}
+
+// 롤링페이퍼 생성 POST /papers
 export interface PaperRequest {
   name: string;
   description: string;
   openDate: string;
 }
 
-export interface PaperResponse {
+export interface PaperCreateResponse {
   name: string;
   description: string;
   openDate: string;
   link: string;
 }
 
+export interface ApiResponsePaperCreateResponse {
+  data: PaperCreateResponse;
+  status: number;
+  message: string;
+}
+
+// 롤링페이퍼 단건 조회 GET /papers/invite
+export interface PaperDetailResponse {
+  paperId: number;
+  name: string;
+  description: string;
+  participantsCount: number;
+  messageCount: number;
+  openDate: string;
+}
+export interface ApiResponsePaperResponse {
+  data: PaperDetailResponse;
+  status: number;
+  message: string;
+}
+
+// 롤링페이퍼 참여 POST /papers/join
 export interface JoinPaperRequest {
   invitationCode: string;
 }
