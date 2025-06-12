155fd4eba66991e4f8f9d48b56cbc1911e948216
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-12 22:53:30 +0900

    fix(containers): edit naming

--- apps/client/src/containers/landingPage/FAQSection.tsx
+++ apps/client/src/containers/landingPage/FAQSection.tsx
@@ -2,21 +2,21 @@
 
 import { Typography } from '@ui/components';
 import { Accordian } from '@ui/components/Accordian';
-import { FAQList } from '@/common/constants/faq';
+import { FAQ_LIST } from '@/common/constants/landing/faq';
 
 export const FAQSection = () => {
   return (
-    <div className="flex flex-col gap-10 items-center mt-[400px] text-center w-full">
+    <section className="flex flex-col gap-10 items-center text-center w-full">
       <Typography variant={'h32-bold'} className="mb-5">
         FAQ
       </Typography>
-      {FAQList.map((m) => (
+      {FAQ_LIST.map((faqItem) => (
         <Accordian
-          key={m.key}
-          question={m.question}
-          answer={m.answer}
+          key={faqItem.id}
+          question={faqItem.question}
+          answer={faqItem.answer}
         ></Accordian>
       ))}
-    </div>
+    </section>
   );
 };
