0aacc615268cfdaa30a1b64bb3f69e6436672af5
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-12 13:49:27 +0900

    feat(page): add containers to landing page

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -6,11 +6,28 @@ import Link from 'next/link';
 import { DetailFunctions } from '@/containers/landing/DetailFunction';
 import { Review } from '@/interface/landing/review.interface';
 import ReviewSectionContainer from '@/containers/landing/ReviewSection';
+import Image from 'next/image';
+import { Accordian } from '@ui/components/Accordian';
+import { IntroSection } from '@/containers/landingPage/IntroSection';
+import { FAQSection } from '@/containers/landingPage/FAQSection';
 
 function Home() {
   return (
     <CommonLayout isLogoVisible>
-      <Typography variant="h36-bold">메인 페이지</Typography>
+      {/* 인트로 섹션 */}
+      <IntroSection />
+
+      {/* 핵심기능 설명 섹션 */}
+      <DetailFunctions />
+
+      {/* 사용자 후기 섹션 */}
+      <Typography variant="h24-bold">돌돌's tory</Typography>
+      <ReviewSectionContainer />
+
+      {/* FAQ 섹션 */}
+      <FAQSection />
+
+      {/* <Typography variant="h36-bold">메인 페이지</Typography>
       <div className="flex flex-col gap-4 mt-8">
         <Typography variant="h24-bold">로그인 전용 페이지</Typography>
         <Link href={'/auth/login'}>
@@ -46,13 +63,6 @@ function Home() {
             로그인 페이지 가기
           </Button>
         </Link> */}
-
-        {/* 핵심기능 설명 섹션 */}
-        <DetailFunctions />
-        {/* 사용자 후기 섹션 */}
-        <Typography variant="h24-bold">돌돌's tory</Typography>
-        <ReviewSectionContainer />
-      </div>
     </CommonLayout>
   );
 }
