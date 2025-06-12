d1bb60bb36c0002ef11020b5c677c583ad680f7a
Leechaeun1 <lce001105@gmail.com>
2025-06-12 11:18:13 +0900

    feat(landing):update gif to video

--- apps/client/src/containers/landing/DetailFunction.tsx
+++ apps/client/src/containers/landing/DetailFunction.tsx
@@ -8,14 +8,16 @@ const sections = [
   {
     key: 'timecapsule',
     label: '타임캡슐',
-    image: '/assets/logos/timecapsule.gif',
+    videoPath: '/assets/logos/timecapsulevideo.mp4',
+    type: 'video',
     description:
       '소중한 이에게 보내는 메시지! 설정한 특정 조건이 충족될 때까지 안전하게 보관하는 타임캡슐 기능입니다.',
   },
   {
     key: 'customizing',
     label: '커스터마이징',
-    image: '/assets/logos/customizing.gif',
+    videoPath: '/assets/logos/customizingvideo.mp4',
+    type: 'video',
     description:
       '소중한 메시지에 어울리는 글씨체와 배경색으로 특별함을 더할 수 있습니다.',
   },
@@ -23,6 +25,7 @@ const sections = [
     key: 'makingkit',
     label: '실물 키트 제작',
     image: '/assets/logos/preparingforservice.png',
+    type: 'image',
     description:
       '소중한 롤링페이퍼를 필름 롤처럼 돌돌 말아 영원히 간직할 수 있는 특별한 굿즈로 제작해 드립니다.',
   },
@@ -50,17 +53,28 @@ export function DetailFunctions() {
 
       {current && (
         <div className="flex gap-8">
-          <Image
-            src={current.image}
-            alt={current.label}
-            width={300}
-            height={650}
-            className="w-48 h-auto rounded-xl shadow-md"
-          />
+          {current.type === 'video' ? (
+            <video
+              src={current.videoPath}
+              autoPlay
+              loop
+              width={300}
+              height={650}
+              className="w-48 h-auto rounded-xl shadow-md"
+            ></video>
+          ) : (
+            <Image
+              src={current.image as string}
+              alt={current.label}
+              width={300}
+              height={650}
+              className="w-48 h-auto rounded-xl shadow-md"
+            />
+          )}
           <div className="text-center flex-grow">
             <Typography
               variant="b14"
-              className="mt-32 text-gray-600 max-w-xl mx-auto md:mt- md:flex-grow"
+              className="mt-28 text-gray-600 max-w-xl mx-auto md:mt- md:flex-grow"
             >
               {current.description}
             </Typography>
