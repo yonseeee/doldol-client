455d438c58fba13f2d0cd01317c6308699abc687
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-13 15:17:13 +0900

    chore(app):  format code with Prettier

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,30 +1,26 @@
-'use client';
+"use client";
 
-import { Button, Typography } from '@ui/components';
-import { CommonLayout } from '@/components/layout/CommonLayout';
-import Link from 'next/link';
-import { DetailFunctions } from '@/containers/landing/DetailFunction';
-import { Review } from '@/interface/landing/review.interface';
-import ReviewSectionContainer from '@/containers/landing/ReviewSection';
-import Image from 'next/image';
-import { Accordian } from '@ui/components/Accordian';
-import { IntroSection } from '@/containers/landingPage/IntroSection';
-import { FAQSection } from '@/containers/landingPage/FAQSection';
+import { Button, Typography } from "@ui/components";
+import { CommonLayout } from "@/components/layout/CommonLayout";
+import Link from "next/link";
+import { DetailFunctions } from "@/containers/landing/DetailFunction";
+import { Review } from "@/interface/landing/review.interface";
+import ReviewSectionContainer from "@/containers/landing/ReviewSection";
+import Image from "next/image";
+import { Accordian } from "@ui/components/Accordian";
+import { IntroSection } from "@/containers/landing/IntroSection";
+import { FAQSection } from "@/containers/landing/FAQSection";
 
 function Home() {
   return (
     <CommonLayout isLogoVisible>
-      {/* 인트로 섹션 */}
       <IntroSection />
 
-      {/* 핵심기능 설명 섹션 */}
       <DetailFunctions />
 
-      {/* 사용자 후기 섹션 */}
       <Typography variant="h24-bold">돌돌's tory</Typography>
       <ReviewSectionContainer />
 
-      {/* FAQ 섹션 */}
       <FAQSection />
 
       {/* <Typography variant="h36-bold">메인 페이지</Typography>
