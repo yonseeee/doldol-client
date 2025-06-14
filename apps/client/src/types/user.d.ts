0aefd76a0147b7ec944df26e8fe3fc48f5e377ca
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-15 02:37:47 +0900

    refactor(types): change socialType from string to socialType

--- apps/client/src/types/user.d.ts
+++ apps/client/src/types/user.d.ts
@@ -1,10 +1,12 @@
+import { SocialType } from "@/enum/social.enum";
+
 // 본인정보 조회 GET /user/info
 export interface MyInfoResponse {
   name: string;
   phone: string;
   email: string;
   socialId: string;
-  socialType: string;
+  socialType: SocialType;
 }
 
 // 개인정보 수정 PATCH /user/info
