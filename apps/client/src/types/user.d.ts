72394ae771413dbdf62bcb0fd8a5fad7b0f304f2
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-14 15:30:21 +0900

    refactor(types): edit user.d.ts

--- apps/client/src/types/user.d.ts
+++ apps/client/src/types/user.d.ts
@@ -1,8 +1,13 @@
-export interface User {
-  id: string;
+// 본인정보 조회 GET /user/info
+export interface MyInfoResponse {
   name: string;
+  phone: string;
+  email: string;
+  socialId: string;
+  socialType: string;
 }
 
+// 개인정보 수정 PATCH /user/info
 export interface UpdateUserInfoRequest {
   name: string;
   password: string;
