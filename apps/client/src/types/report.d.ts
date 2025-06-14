943ac61bcd857999348ab2996bd62967ef9618ed
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-14 15:30:11 +0900

    refactor(types): edit report.d.ts

--- apps/client/src/types/report.d.ts
+++ apps/client/src/types/report.d.ts
@@ -7,14 +7,17 @@ export interface ReportRequest {
 
 export interface ReportResponse {
   messageId: number;
+  messageContent: string;
   title: string;
   content: string;
   createdAt: string;
   isAnswered: boolean;
 }
 
-export interface ApiResponseListReportResponse {
-  data: ReportResponse[];
-  status: number;
-  message: string;
+export interface ReportListResponse {
+  list: ReportResponse[];
+  size: number;
+  nextCursor: number;
+  hasNext: boolean;
+  empty: boolean;
 }
