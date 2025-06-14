428f8d1db7a4646b7766b9f8bf7c3b5b41f27d7b
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-15 02:40:49 +0900

    feat(services): add user.ts

--- apps/client/src/services/user.ts
+++ apps/client/src/services/user.ts
@@ -1,6 +1,11 @@
 import { ApiResponseMyInfo } from "@/types/user";
 import { apiClient } from "./apiClient";
+import { EditProfileInputForm } from "@/interface/my-page/edit-profile/edit.interface";
 
 export const getUserInfo = () => {
   return apiClient.get<ApiResponseMyInfo>("/user/info");
+};
+
+export const patchUserInfo = (data: EditProfileInputForm) => {
+  return apiClient.patch("/user/info", data);
 };
