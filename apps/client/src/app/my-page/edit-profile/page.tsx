eb49c39f4a2fcdcffbf01d85bd3026985fca57fe
seyeon_bb@naver.com <seyeon_bb@naver.com>
2025-06-16 17:42:36 +0900

    fix(edit-profile): remove unnecessary codes

--- apps/client/src/app/my-page/edit-profile/page.tsx
+++ apps/client/src/app/my-page/edit-profile/page.tsx
@@ -2,11 +2,8 @@
 
 import React, { useEffect } from "react";
 import dynamic from "next/dynamic";
-import { EditProfileInputForm } from "@/interface/my-page/edit-profile/edit.interface";
 import { SocialType } from "@/enum/social.enum";
 import { getUserInfo } from "@/services/user";
-import { MyInfoResponse } from "@/types/user";
-// import { Router, useRouter } from "next/router";
 
 const Content = {
   input: dynamic(
@@ -19,19 +16,9 @@ const Content = {
 };
 
 const EditProfilePage = () => {
-  const [userData, setUserData] = React.useState<MyInfoResponse | undefined>(
-    undefined
-  );
-
   const dummyUser = {
     name: "홍길동",
-    socialType: SocialType.Kakao, // 또는 Google, Naver
-  };
-
-  //   const router = useRouter();
-  const onNext = () => {
-    console.log("수정 완료됨 → 마이페이지 이동 예정");
-    // router.push("/my-page");
+    socialType: SocialType.Kakao,
   };
 
   //   TODO: API연동 후 주석 제거
@@ -39,14 +26,7 @@ const EditProfilePage = () => {
   //     getUserInfo().then((res) => setUserData(res.data));
   //   });
 
-  //   TODO: 추후 user의 socialType으로 변경
-  //   return (
-  //     <>{<Content.input onNext={onNext} socialType={userData?.socialType} />}</>
-  //   );
-
-  return (
-    <>{<Content.input onNext={onNext} socialType={dummyUser.socialType} />}</>
-  );
+  return <>{<Content.input socialType={dummyUser.socialType} />}</>;
 };
 
 export default EditProfilePage;
