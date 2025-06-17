cc3258fe5418f3968e705576cce0ad1df6660939
Leechaeun1 <lce001105@gmail.com>
2025-06-17 15:28:54 +0900

    fix(design): edit messageStyle.ts and remove profile image border

--- apps/client/src/utils/messageStyle.ts
+++ apps/client/src/utils/messageStyle.ts
@@ -1,5 +1,15 @@
 export function getTextColor(bg: string): string {
-  const darkColors = ["primary-brand", "purple"];
+  // 여기에 포함 되어있으면
+  const darkColors = [
+    "red-1",
+    "green-1",
+    "primary-dark-1",
+    "primary-dark-2",
+    "black",
+    "gray-1",
+    "gray-2",
+    "primary-brand",
+  ];
   return darkColors.includes(bg) ? "text-white" : "text-black";
 }
 
@@ -7,7 +17,20 @@ export function getBgColorClass(bg: string): string {
   return (
     {
       "primary-brand": "bg-primary-brand",
+      "primary-light1": "bg-primary-light1",
+      "primary-dark1": "bg-primary-dark1",
+      "primary-dark2": "bg-primary-dark2",
       "green-1": "bg-green-1",
+      "purple-1": "bg-purple-1",
+      "red-1": "bg-red-1",
+      "yellow-1": "bg-yellow-1",
+      black: "bg-black",
+      "gray-1": "bg-gray-1",
+      "gray-2": "bg-gray-2",
+      "gray-3": "bg-gray-3",
+      "gray-4": "bg-gray-4",
+      "gray-5": "bg-gray-5",
+      white: "bg-white",
       green: "bg-green-400",
       purple: "bg-purple-400",
       yellow: "bg-yellow-300",
