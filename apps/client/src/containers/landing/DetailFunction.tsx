ffb77d1359f10387219f70ad0dfd653193ee55b6
Leechaeun1 <lce001105@gmail.com>
2025-06-21 00:41:42 +0900

    fix(design): edit accordian ui & detaile description

--- apps/client/src/containers/landing/DetailFunction.tsx
+++ apps/client/src/containers/landing/DetailFunction.tsx
@@ -46,7 +46,7 @@ export function DetailFunctions() {
               className="w-48 h-auto rounded-xl shadow-md"
             />
           )}
-          <div className="text-center flex-grow">
+          <div className="flex-grow text-left">
             <Typography variant="b15" className="mt-32">
               {current.description}
             </Typography>
--- packages/ui/src/components/Accordian/Accordian.tsx
+++ packages/ui/src/components/Accordian/Accordian.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { ArrowSLineDown } from '@icons/ArrowSLineDown';
+import { ArrowSLineUp } from '@icons/ArrowSLineUp';
 import { Icon } from '../Icon';
 import React from 'react';
 import { Typography } from '../Typography';
@@ -24,11 +25,11 @@ export const Accordian: React.FC<Props> = ({ question, answer }) => {
         <Typography variant={'b20-bold'} className="text-start mx-4">
           {question}
         </Typography>
-        <Icon icon={ArrowSLineDown} size={24} className="mx-4" />
+        <Icon icon={isOpen ? ArrowSLineUp : ArrowSLineDown} size={24} className="mx-4" />
       </button>
 
       {isOpen && (
-        <div className="bg-primary">
+        <div className="bg-primary mx-4 text-left">
           <Typography variant={'b16'}>{answer}</Typography>
         </div>
       )}
