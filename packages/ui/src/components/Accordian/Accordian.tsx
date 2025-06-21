d8b5282dba537bc123f1c9e151a0796b627e7860
Leechaeun1 <lce001105@gmail.com>
2025-06-22 01:16:12 +0900

    fix(design): add effect

--- packages/ui/src/components/Accordian/Accordian.tsx
+++ packages/ui/src/components/Accordian/Accordian.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ArrowSLineDown } from '@icons/ArrowSLineDown';
-import { ArrowSLineUp } from '@icons/ArrowSLineUp';
 import { Icon } from '../Icon';
 import React from 'react';
 import { Typography } from '../Typography';
