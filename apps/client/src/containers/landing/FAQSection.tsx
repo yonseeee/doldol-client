641e87c747b419783bd14b17e1ba6c639c548d6c
Leechaeun1 <lce001105@gmail.com>
2025-06-26 15:30:36 +0900

    add

--- apps/client/src/containers/landing/FAQSection.tsx
+++ apps/client/src/containers/landing/FAQSection.tsx
@@ -6,10 +6,21 @@ import { Typography } from "@ui/components";
 
 export const FAQSection = () => {
   return (
-    <section className="flex flex-col gap-10 items-center text-center w-full mb-40">
-      <Typography variant={"h32-bold"} className="mb-5">
-        FAQ
-      </Typography>
+    <section className="flex flex-col gap-10 w-full mb-40">
+      <div className="relative flex justify-center mb-5">
+        <img
+          src="/assets/images/sh.png"
+          alt="FAQ Icon"
+          className="w-40 absolute left-0 top-1/2 transform -translate-y-1/2"
+        />
+        <Typography variant={"h32-bold"}>FAQ</Typography>
+        <img
+          src="/assets/images/jw.png"
+          alt="FAQ Icon"
+          className="w-40 absolute right-0 top-1/2 transform -translate-y-1/2"
+        />
+      </div>
+
       {FAQ_LIST.map((faqItem) => (
         <Accordian
           key={faqItem.id}
