d8501c2e992558fed0cee6ee7424c64b81147b0b
Leechaeun1 <lce001105@gmail.com>
2025-07-01 10:27:08 +0900

    feat(seo): environmental separation

--- apps/client/src/app/robots.ts
+++ apps/client/src/app/robots.ts
@@ -1,6 +1,15 @@
 import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
+  if (process.env.NODE_ENV === "development") {
+    return {
+      rules: {
+        userAgent: "*",
+        disallow: ["/"],
+      },
+    };
+  }
+
   return {
     rules: {
       userAgent: "*",
--- apps/client/src/app/sitemap.ts
+++ apps/client/src/app/sitemap.ts
@@ -1,6 +1,10 @@
 import type { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
+  if (process.env.NODE_ENV === "development") {
+    return [];
+  }
+
   return [
     {
       url: "https://doldol.wha1eson.co.kr/",
