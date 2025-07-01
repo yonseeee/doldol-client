b90a1b13d3b0acdbcff71a33f2c7d5dd4506e71a
Leechaeun1 <lce001105@gmail.com>
2025-07-01 10:27:06 +0900

    fix(robots): using prettier

--- apps/client/src/app/robots.ts
+++ apps/client/src/app/robots.ts
@@ -1,12 +1,12 @@
-import type { MetadataRoute } from 'next'
- 
+import type { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: ['/auth/','/paper/','/my-page/']
+      userAgent: "*",
+      allow: "/",
+      disallow: ["/auth/", "/paper/", "/my-page/"],
     },
-    sitemap: 'https://doldol.wha1eson.co.kr//sitemap.xml',
-  }
+    sitemap: "https://doldol.wha1eson.co.kr//sitemap.xml",
+  };
 }
